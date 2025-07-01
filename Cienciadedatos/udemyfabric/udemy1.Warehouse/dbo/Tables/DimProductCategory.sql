db80cc7ecb0698a5ec3f387b9df74178c66e687d
JUAN HERIBERTO ROSAS JUAREZ <152189440+gobiernodigitaleinnovacion@users.noreply.github.com>
2025-07-01 10:15:11 -0600

    Committing 173 items from workspace 0f0fe17e-40bb-46fa-bc10-23fdcd6b597b

--- Cienciadedatos/udemyfabric/udemy1.Warehouse/dbo/Tables/DimProductCategory.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[DimProductCategory] (
-
-	[ProductCategoryKey] bigint NULL, 
-	[ProductCategoryAlternateKey] bigint NULL, 
-	[EnglishProductCategoryName] varchar(8000) NULL, 
-	[SpanishProductCategoryName] varchar(8000) NULL, 
-	[FrenchProductCategoryName] varchar(8000) NULL
-);
--- Cienciadedatos/udemyfabric/udemy1.Warehouse/dbo/Tables/DimProductSubcategory.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[DimProductSubcategory] (
-
-	[ProductSubcategoryKey] bigint NULL, 
-	[ProductSubcategoryAlternateKey] bigint NULL, 
-	[EnglishProductSubcategoryName] varchar(8000) NULL, 
-	[SpanishProductSubcategoryName] varchar(8000) NULL, 
-	[FrenchProductSubcategoryName] varchar(8000) NULL, 
-	[ProductCategoryKey] bigint NULL
-);
